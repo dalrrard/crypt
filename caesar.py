8a32b03cc74e93b97127f8bd18b5b7622bacc094
dalrrard <dalton.rardin@gmail.com>
2015-09-05 14:52:11 -0500

    removed test for letters

--- caesar.py
+++ caesar.py
@@ -2,15 +2,12 @@ __author__ = 'dalton'
 
 message = input('Enter message: ').upper()
 enc_or_dec = input('(E)ncrypt or (d)ecrypt? ').lower()
-key = input('Enter key: ')
+key = int(input('Enter key: '))
 result = ''
 
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-if not message.isalpha() or key.isalpha():
-    print("Only letters for message and numbers for key, please")
-elif enc_or_dec in ('encrypt', 'e'):
-    key = int(key)
+if enc_or_dec in ('encrypt', 'e'):
     for i in message:
         if i in letters:
             num = letters.find(i)
@@ -21,7 +18,6 @@ elif enc_or_dec in ('encrypt', 'e'):
         else:
             result += letters[(num+key) - len(letters)]
 elif enc_or_dec in ('decrypt', 'd'):
-    key = int(key)
     for i in message:
         if i in letters:
             num = letters.find(i)
